3223e740d47ad53f07559ca08b72cf72f88302a0
Patrick Jensen <73653096+pmjens@users.noreply.github.com>
2025-03-17 16:35:29 -0700

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,20 +17,6 @@ function Navbar() {
             <li className="nav-item">
               <Link className="nav-link" to="/search">Find Scholarships</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/applications">My Applications</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/essay-assistant">Essay Assistant</Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/profile">Profile</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
           </ul>
         </div>
       </div>
