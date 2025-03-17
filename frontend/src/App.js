31f822ea48661fafebab295272ca9ace4e4618bc
Patrick Jensen <73653096+pmjens@users.noreply.github.com>
2025-03-17 16:34:15 -0700

    Update App.js
    Fix App.js to include only implemented pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ScholarshipSearch from './pages/ScholarshipSearch';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import StudentProfile from './pages/StudentProfile';
-import ApplicationTracker from './pages/ApplicationTracker';
-import EssayAssistant from './pages/EssayAssistant';
 import './styles.css';
 
-function App() {
+function App()  {
   return (
     <Router>
       <div className="app-container">
@@ -19,11 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<ScholarshipSearch />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<StudentProfile />} />
-            <Route path="/applications" element={<ApplicationTracker />} />
-            <Route path="/essay-assistant" element={<EssayAssistant />} />
           </Routes>
         </div>
       </div>
